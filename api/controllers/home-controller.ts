9a129be9b7abe47da7fe164137864661486ecde3
lalankeba <lalankeba@gmail.com>
2024-09-13 10:45:43 +0530

    updating home

--- api/controllers/home-controller.ts
+++ api/controllers/home-controller.ts
@@ -4,8 +4,8 @@ import * as homeService from "../services/home-service";
 
 const init = async (req: Request, res: Response, next: NextFunction) => {
   try {
-      logger.info(`Initilizing...`);
       const homeResp = await homeService.init();
+      logger.info(`API is working`);
       res.status(200).json(homeResp);
   } catch (err) {
       next(err);
--- api/services/home-service.ts
+++ api/services/home-service.ts
@@ -1,7 +1,4 @@
-import logger from "../config/logger-config";
-
 const init = async (): Promise<Object> => {
-  logger.info(`init`);
   return {"msg": "Welcome to sports data analyser API"};
 }
 
