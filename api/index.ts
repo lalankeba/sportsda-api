52c7e64d56fd705b1370cfdcf39a2d65ee8cabd0
lalankeba <lalankeba@gmail.com>
2024-09-13 10:46:30 +0530

    adding error handling

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,8 @@ import logger from './config/logger-config';
 import rateLimiter from './config/rate-limiter';
 import RequestLogger from './middleware/request-logger';
 import homeRoute from './routes/home-route';
+import notFoundHandler from './middleware/not-found-handler';
+import errorHandler from './middleware/error-handler';
 
 const app = express();
 const PORT: number = parseInt(process.env.PORT || '3010', 10);
@@ -22,6 +24,10 @@ app.use(RequestLogger);
 app.use(express.json());
 
 app.use('/', homeRoute);
+
+// handling errors
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   logger.info(`App is running on port ${PORT}`);
